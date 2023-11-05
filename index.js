cba6859fce27e2618dff75dbb0c19b89acaa9df9
dimaboi21 <boidima21@gmail.com>
2023-11-05 12:45:35 +0200

    menu

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-function startTimer() {
-  let minutes = 1;
-  let seconds = 10;
-  const timerElement = document.getElementById("timer");
-  let timerInterval = setInterval(function () {
-    timerElement.textContent = formatTime(minutes) + ":" + formatTime(seconds);
-    seconds--;
-    if (minutes === 0 && seconds === 0) {
-      clearInterval(timerInterval);
-      timerElement.textContent = "00:00";
-    } else if (seconds < 0) {
-      minutes--;
-      seconds = 59;
-    }
-  }, 1000);
-  function formatTime(time) {
-    return time < 10 ? "0" + time : time;
-  }
-}
-window.onload = startTimer;
+const menuBtn = document.querySelector('.menu-btn')
+const menuList = document.querySelector('.nav__list')
+
+menuBtn.addEventListener("click" , () => {
+    menuList.classList.toggle("nav__active")
+})
+
+menuBtn.addEventListener("click" , () =>{
+    menuBtn.classList.toggle("menu__active")
+})
+
+// function startTimer() {
+//   let minutes = 1;
+//   let seconds = 10;
+//   const timerElement = document.getElementById("timer");
+//   let timerInterval = setInterval(function () {
+    
+//     timerElement.textContent = formatTime(minutes) + ":" + formatTime(seconds);
+//     seconds--;
+//     if (minutes === 0 && seconds === 0) {
+//       clearInterval(timerInterval);
+//       timerElement.textContent = "00:00";
+//     } else if (seconds < 0) {
+//       minutes--;
+//       seconds = 59;
+//     }
+//   }, 1000);
+//   function formatTime(time) {
+//     return time < 10 ? "0" + time : time;
+//   }
+// }
+// window.onload = startTimer;
